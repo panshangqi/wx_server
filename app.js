18434e61686f2f66377c16f6afdde781839362ef
panshangqi <1187816874@qq.com>
2021-02-21 14:32:08 +0800

    增加流媒体

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const fs = require('fs')
 const staticFiles = require('koa-static')
 
 const app = new Koa()
+const mime = {
+    'mp4': 'video/mp4',
+    'webm': 'video/webm',
+    'ogg': 'application/ogg',
+    'ogv': 'video/ogg',
+    'mpg': 'video/mepg',
+    'flv': 'flv-application/octet-stream',
+    'mp3': 'audio/mpeg',
+    'wav': 'audio/x-wav'
+}
 app.use(staticFiles(path.join(__dirname + '/static/')))
 app.use(staticFiles(path.join(__dirname + '../files/')))
 router.get('/',(ctx)=>{
@@ -15,9 +25,32 @@ router.get('/',(ctx)=>{
 
 app.use(router.routes())//启动路由
 
-const port = 9999;
+const port = 9000;
 async function startApp() {
     app.listen(port);
     console.log('server is start, port='+port)
 }
 startApp()
+
+
+//流媒体服务
+//==================================================
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 30,
+        ping_timeout: 60
+    },
+    http: {
+        port: 9999,
+        mediaroot: './static/', // 建议写
+        allow_origin: '*'
+    }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
